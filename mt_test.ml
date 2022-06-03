438ab718bd9e5c38bdad8b0206e20538c16980e4
Noty <yuki.kamata1001@gmail.com>
2022-06-03 20:54:05 +0900

    Update mt_test.ml

--- mt_test.ml
+++ mt_test.ml
@@ -1,6 +1,5 @@
 open Mersenne_twister
 open Printf
-
 let _ =
 	let mt = init_by_array [| 0x123l; 0x234l; 0x345l; 0x456l |] in
 
@@ -9,11 +8,9 @@ let _ =
 		printf "%10lu " (genrand_int32 mt);
 		if i mod 5 = 4 then printf "\n"
 	done;
-
-	printf "\n1000 outputs of genrand_real2()\n";
+printf "\n1000 outputs of genrand_real2()\n";
 	for i = 0 to 999 do
 		printf "%10.8f " (genrand_real2 mt);
 		if i mod 5 = 4 then printf "\n"
 	done;
-
-	exit 0
+exit 0
